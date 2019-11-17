824de1de1354500babda8cecf8056f69262b0d78
Emily Palmer <emilymmpalmer@gmail.com>
2019-11-16 21:26:31 -0800

    edit pumpkin colors

--- R/cat_colors.R
+++ R/cat_colors.R
@@ -10,10 +10,10 @@ cat_colors <- list(
       "#a17652", "#ad835f", "#b9916a", "#c79e71",
       "#d4ac78", "#debb8a", "#e7ca9d", "#f0d9b1", "#f7e9c6"),
         1:9),
-  Pumpkin = rbind(c(
-      "#c07f53", "#d38e5e", "#daa272",
-      "#e2b68d", "#e8c9aa", "#eddcca", "#f1f0ec"),
-      c(4,1,6,2,5,7,3)),
+  Pumpkin = rbind(c('#440600', '#5c2100', '#763800',
+                     '#8e4e25', '#a66540', '#bf7d52', '#d38e5e',
+                     "#e2b68d", "#e8c9aa", "#eddcca", "#f1f0ec"
+                     ),11:1),
   Nicholas = rbind(c(
       "#2d2926", "#3f3f3e", "#525558",
       "#666d72", "#c3aab1", "#facbe9"),
